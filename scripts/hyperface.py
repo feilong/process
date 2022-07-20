040d27177bce0e5d44b4775b5012f2dd8abe220d
Ma Feilong <mafeilong@gmail.com>
2022-07-20 17:47:46 -0400

    Nuisance regression and renaming.

--- scripts/hyperface.py
+++ scripts/hyperface.py
@@ -1,7 +1,46 @@
 import os
 import sys
+import json
 from glob import glob
 from process.fmriprep import run_fmriprep
+from process.regression import regression_workflow
+
+
+def rename_func(fn):
+    timing_dir = os.path.expanduser('~/hyperface/timing')
+    hyperface_runs = [2, 3, 4, 6, 7, 8]
+
+    info = {}
+    chunks = os.path.basename(fn).split('_')
+    for chunk in chunks[:-1]:
+        key, val = chunk.split('-')
+        info[key] = val
+    info['lr'] = chunks[-1][0]
+
+    for key in info:
+        if key not in ['sub', 'ses', 'task', 'run', 'lr']:
+            raise ValueError
+    sid, ses, task, run, lr = info['sub'], info['ses'], info['task'], info['run'], info['lr']
+
+    if task == 'localizer':
+        key = (ses, run)
+        new_run = {('1', '01'): 1, ('1', '05'): 2, ('2', '01'): 3, ('2', '05'):4}[key]
+        new_task = 'localizer'
+    elif task == 'visualmemory':
+        run_ = hyperface_runs.index(int(run)) + 1
+        with open(f'{timing_dir}/sub-{sid}_order_runs.json', 'r') as f:
+            res = json.load(f)
+        new_run = int(res[f'ses-{ses}_run-{run_}']['order_orig'])
+        new_task = 'hyperface'
+    elif ses == 'budapest' and task == 'movie':
+        new_run = int(run)
+        assert new_run <= 5
+        new_task = 'budapest'
+    else:
+        raise ValueError
+
+    label = f'{sid}_{new_task}_{new_run:02d}_{lr}h'
+    return label
 
 
 if __name__ == '__main__':
@@ -47,6 +86,11 @@ if __name__ == '__main__':
             '--use-syn-sdc',
             '--output-spaces', 'fsaverage5',
         ],
+
+        'resample_flavor': 'on-avg-1031-final_ico32_2step_normals_equal',
     }
 
     run_fmriprep(config, cleanup=True)
+
+    out_dir = os.path.expanduser(f'~/singularity_home/data/haxbylab-siemens_{fmriprep_version}/{config["resample_flavor"]}/no-gsr')
+    regression_workflow(config, out_dir, rename_func, n_jobs=n_procs)
