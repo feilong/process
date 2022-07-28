5e21111814b08687ec4047c4b55242a3f1ed5779
Ma Feilong <mafeilong@gmail.com>
2022-07-28 16:02:54 -0400

    Perform nuisance regression for the Forrest dataset.

--- scripts/forrest.py
+++ scripts/forrest.py
@@ -2,6 +2,34 @@ import os
 import sys
 from glob import glob
 from process.fmriprep import run_fmriprep
+from process.regression import regression_workflow
+
+
+def rename_func(fn):
+    info = {}
+    chunks = os.path.basename(fn).split('_')
+    for chunk in chunks[:-1]:
+        key, val = chunk.split('-')
+        info[key] = val
+    info['lr'] = chunks[-1][0]
+
+    for key in info:
+        if key not in ['sub', 'ses', 'task', 'run', 'lr']:
+            raise ValueError
+    sid, ses, task, run, lr = info['sub'], info['ses'], info['task'], info['run'], info['lr']
+
+    if ses == 'movie' and task == 'movie':
+        new_run = int(run)
+        new_task = 'forrest'
+    elif task in ['objectcategories', 'retmapccw', 'retmapclw', 'retmapcon', 'retmapexp', 'movielocalizer']:
+        new_run = int(run)
+        new_task = task
+    else:
+        raise ValueError
+
+    label = f'{sid}_{new_task}_{new_run:02d}_{lr}h'
+    return label
+
 
 
 if __name__ == '__main__':
@@ -48,6 +76,13 @@ if __name__ == '__main__':
             '--use-syn-sdc',
             '--output-spaces', 'fsaverage5',
         ],
+
+        'resample_flavor': 'on-avg-1031-final_ico32_2step_normals_equal',
     }
 
     run_fmriprep(config, cleanup=True)
+
+    out_dir = os.path.expanduser(f'~/singularity_home/data/final/forrest_{fmriprep_version}/onavg-ico32/2step_normals_equal/no-gsr')
+    regression_workflow(config, out_dir, rename_func, n_jobs=n_procs, resample_flavor='on-avg-1031-final_ico32_2step_normals_equal', ignore_non_existing=True)
+    out_dir = os.path.expanduser(f'~/singularity_home/data/final/forrest_{fmriprep_version}/fsavg-ico32/2step_normals_equal/no-gsr')
+    regression_workflow(config, out_dir, rename_func, n_jobs=n_procs, resample_flavor='fsaverage_ico32_2step_normals_equal', ignore_non_existing=True)
